361747f6e9710883986e1dd49fa464def4bb47fe
idallas93 <Dallas.Isaac1@gmail.com>
2020-10-29 16:21:49 -0400

    added delete function to server.js

--- server.js
+++ server.js
@@ -50,5 +50,30 @@ app.post("/api/notes", function (req, res) {
         status: 200,
         success: true
     });
+    
+});
 
+app.delete("/api/notes/:id", function (req, res) {
+    // id is index of note in notesArray
+    let id = parseInt(req.params.id);
+    // Use id index to remove item from notesArray
+    let removeItemArray = notesArray.filter(item => item.id != id);
+
+    removeItemArray.forEach(element => element.id = removeItemArray.indexOf(element));
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(removeItemArray));
+
+    res.json({
+        isError: false,
+        message: "Note successfully deleted",
+        port: PORT,
+        status: 200,
+        success: true
+    });
+});
+
+
+// Redirect to root if no routes match
+app.get("*", function (req, res) {
+    res.redirect('/');
 });
